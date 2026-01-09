6e1a5eefb09c94a3479f38de1854a32be2c3d1eb
CkriT <ckrit3@gmail.com>
2026-01-10 06:48:41 +0900

    flask URI 생성 및 parameter test

--- host.py
+++ host.py
@@ -16,6 +16,30 @@ def validateCheck():
 def main():
     return render_template('main.html')
 
+@app.route("/category/<categoryNo>")
+def categoryPage(categoryNo):
+    return render_template('category.html', categoryNo=categoryNo)
+
+@app.route("/contents/<contentsNo>")
+def contentsPage(contentsNo):
+    return render_template('contetns.html', contentsNo=contentsNo)
+
+@app.route("/search/<keyword>")
+def searchPage(keyword):
+    return render_template('search.html', keyword=keyword)
+
+@app.route("/join")
+def joinPage():
+    return render_template('join.html')
+
+@app.route("/find")
+def findPage():
+    return render_template('find.html')
+
+@app.route("/user/<userNo>")
+def userPage(userNo):
+    return render_template('user.html', userNo=userNo)
+
 if __name__ == '__main__':
     app.run(
         debug=True,
