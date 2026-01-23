3247d9732a9cfc913f70aac69b0348c25bf98a29
CkriT <ckrit3@gmail.com>
2026-01-24 00:48:59 +0900

    DTO DAO는 이정도면 정리 된 것 같다..

--- dao/commentDAO.py
+++ dao/commentDAO.py
@@ -1,30 +1,15 @@
 from dto import commentDTO
 from service import db, store
 
-'''
-댓글 달기
-parameter: 댓글객체(commentDTO)
-return: 실패 0, 성공 1(int)
-'''
-def setComment(comment):
-    sql = f'''\
-        INSERT INTO comment(b_no,u_no,co_ip,co_comment,co_upper) \
-        VALUES(\
-            {comment.getBoardNo()},\
-            {comment.getUserNo()},\
-            "{comment.getIp()}",\
-            "{comment.getComment()}",\
-            {comment.getUpper()}
-        )'''
-    result = db.setData(sql=sql)
-    return result
-
-'''
-글번호로 댓글객체 리스트 가져오기
-parameter: 글번호(int)
-return: 댓글객체 리스트(List)
-'''
+#################################################################################################
+###################################### Get Comment Object #######################################
+#################################################################################################
 def getCommentListByBoardNo(bno):
+    '''
+    글번호로 댓글객체 리스트 가져오기
+    parameter: 글번호(int)
+    return: 댓글객체 리스트(List)
+    '''
     commentList = []
     sql = f'''\
         SELECT c.*, u.u_email, u.u_state, b.b_title FROM comment c \
@@ -51,32 +36,22 @@ def getCommentListByBoardNo(bno):
         commentList.append([pc,tempList])
     return commentList
 
-'''
-댓글 삭제 (co_isdelete를 1로 update)
-parameter: 댓글번호(int)
-return: 실패 0, 성공 1(int)
-'''
-def deleteComment(cono):
-    sql = f'''UPDATE comment SET co_isdelete = 1 WHERE co_no = {cono}'''
-    result = db.setData(sql=sql)
-    return result
-
-'''
-유저별 작성한 댓글 갯수 가져오기
-parameter: 유저번호(int)
-return: 작성한 댓글 갯수(int)
-'''
 def getCommentCountByUserNo(uno):
+    '''
+    유저별 작성한 댓글 갯수 가져오기
+    parameter: 유저번호(int)
+    return: 작성한 댓글 갯수(int)
+    '''
     sql = f'''SELECT count(*) FROM comment WHERE u_no = {uno} AND co_isdelete = 0'''
     result = db.getData(sql=sql)[0][0]
     return result
 
-'''
-유저별 작성한 최신댓글 목록 가져오기
-parameter: 유저번호(int)
-return: 댓글 목록 리스트([commentDTO,....])
-'''
 def getRecentlyCommentList(uno):
+    '''
+    유저별 작성한 최신댓글 목록 가져오기
+    parameter: 유저번호(int)
+    return: 댓글 목록 리스트([commentDTO,....])
+    '''
     result = []
     sql = f'''\
         SELECT c.*, u.u_email, u.u_state, b.b_title FROM comment c \
@@ -90,3 +65,38 @@ def getRecentlyCommentList(uno):
         c.setCommentByDbResult(comment)
         result.append(c)
     return result
+
+#################################################################################################
+###################################### Set Comment Object #######################################
+#################################################################################################
+def setComment(comment):
+    '''
+    댓글 달기
+    parameter: 댓글객체(commentDTO)
+    return: 실패 True, 성공 False (bool)
+    '''
+    sql = f'''\
+        INSERT INTO comment(b_no,u_no,co_ip,co_comment,co_upper) \
+        VALUES(\
+            {comment.getBoardNo()},\
+            {comment.getUserNo()},\
+            "{comment.getIp()}",\
+            "{comment.getComment()}",\
+            {comment.getUpper()}
+        )'''
+    result = db.setData(sql=sql)
+    if result == 0:
+        return False
+    return True
+
+'''
+댓글 삭제 (co_isdelete를 1로 update)
+parameter: 댓글번호(int)
+return: 성공 True, 실패 False (bool)
+'''
+def deleteComment(cono):
+    sql = f'''UPDATE comment SET co_isdelete = 1 WHERE co_no = {cono}'''
+    result = db.setData(sql=sql)
+    if result == 0:
+        return False
+    return True
