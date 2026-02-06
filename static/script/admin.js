780859688eef544030070d9b21a38b78fa04e5f4
CkriT <ckrit3@gmail.com>
2026-02-06 18:56:41 +0900

    moveChildCategoryDiv 수정중

--- static/script/admin.js
+++ static/script/admin.js
@@ -3,6 +3,7 @@ let adminDeleteDummyBtn = document.getElementById('adminDeleteDummyBtn')
 let adminRebootBtn = document.getElementById('adminRebootBtn')
 let adminNoticeTextarea = document.getElementById('adminNoticeTextarea')
 let adminNoticeBtn = document.getElementById('adminNoticeBtn')
+let adminCategoryDiv = document.getElementById('adminCategoryDiv')
 
 adminCheckImageBtn.addEventListener('click',()=>{
     fetch("/adminCheckImage", {
@@ -68,3 +69,86 @@ adminNoticeBtn.addEventListener('click',()=>{
             }
         });
 })
+
+function changeCategoryName(target){
+    let newName = prompt("해당 카테고리의 새로운 이름")
+    let childenList = target.cloneNode(true).children
+    if(childenList[0].innerHTML.indexOf('(') != 0){
+        let count = childenList[0].innerHTML.split('(')[1]
+        childenList[0].innerHTML=newName + '(' + count
+    }else{
+        childenList[0].innerHTML=newName
+    }
+    target.innerHTML=''
+    while(childenList.length != 0){
+        target.appendChild(childenList[0])
+        target.innerHTML += ' '
+    }
+}
+
+function moveParentCategoryDiv(parentDiv_id,heading){
+    let parentChildrenList = Array.from(adminCategoryDiv.children)
+    let fromIndex = parseInt(parentDiv_id.split('_')[1]) - 1
+    let toIndex = 0
+    if(heading == 'up'){
+        if(fromIndex === 0){
+            return
+        }
+        toIndex = fromIndex - 1
+    }else{
+        if(fromIndex >= parentChildrenList.length-2){
+            return
+        }
+        toIndex = fromIndex +1
+    }
+    adminCategoryDiv.innerHTML = ''
+    for(let i=0;i<parentChildrenList.length;i++){
+        if(i==toIndex){
+            parentChildrenList[fromIndex].id = "parent_" + (toIndex + 1)
+            adminCategoryDiv.appendChild(parentChildrenList[fromIndex])
+        }else if(i==fromIndex){
+            parentChildrenList[toIndex].id = "parent_" + (fromIndex + 1)
+            adminCategoryDiv.appendChild(parentChildrenList[toIndex])
+        }else{
+            adminCategoryDiv.appendChild(parentChildrenList[i])
+        }
+    }
+}
+
+function moveChildCategoryDiv(parentDiv_id,heading){
+    let grandParentDiv = document.getElementById(parentDiv_id).parentElement
+    let parentChildrenList = Array.from(adminCategoryDiv.children)
+    let idTextList = parentDiv_id.split('_')
+    let fromIndex = parseInt(idTextList[4]) - 1
+    let toIndex = 0
+    let idText = idTextList[0] + '_' + idTextList[1] + '_' + idTextList[2] + '_' + idTextList[3] + '_'
+    if(heading == 'up'){
+        if(fromIndex === 0){
+            return
+        }
+        toIndex = fromIndex - 1
+    }else{
+        if(fromIndex >= parentChildrenList.length-2){
+            return
+        }
+        toIndex = fromIndex +1
+    }
+    grandParentDiv.innerHTML = ''
+    for(let i=0;i<parentChildrenList.length;i++){
+        if(i==toIndex){
+            parentChildrenList[fromIndex].id = idText + (toIndex + 1)
+            grandParentDiv.appendChild(parentChildrenList[fromIndex])
+        }else if(i==fromIndex){
+            parentChildrenList[toIndex].id = idText + (fromIndex + 1)
+            grandParentDiv.appendChild(parentChildrenList[toIndex])
+        }else{
+            grandParentDiv.appendChild(parentChildrenList[i])
+        }
+    }
+}
+
+function deleteCategory(target){
+    if(confirm("카테고리에 저장된 글이 모두 삭제됩니다.")){
+        target.remove()
+    }
+}
